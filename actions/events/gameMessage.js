784dfd174bc3faa31e14dd37b89966d3de698e08
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2023-11-28 20:23:59 +0100

    Update minigame command and database functions

---
+++ actions/events/gameMessage.js
@@ -0,0 +1,21 @@
+const config = require('../../config.json');
+module.exports = {
+    data: {
+        name: 'messageCreate',
+        type: 'on',
+    },
+    run: function (client, functions, connection) {
+        return async function (message) {
+            if (message.author.bot) return;
+
+            const channels = await functions.runQuery(functions, connection, `SELECT * FROM games WHERE channelID = '${message.channel.id}'`);
+            if(channels.length > 0) {
+                const channel = channels[0];
+                if(channel.type === 1) {
+                    functions.runGame1(functions, connection, message, channel);
+                }
+            }
+
+        }
+    }
+}
