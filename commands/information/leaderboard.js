343bb4f94cf017c4d697bb6ee8b824184ad4bb8d
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2024-01-06 18:04:43 +0100

    Update leaderboard command and change status

--- commands/information/leaderboard.js
+++ commands/information/leaderboard.js
@@ -2,21 +2,29 @@ const { ActionRowBuilder, ButtonBuilder } = require('discord.js');
 
 module.exports = {
     data: {
-        name: 'disabled',
+        name: 'leaderboard',
         message: false,
         options: [],
         description: "A minigame",
         permissions: 0,
     },
     run: async function (client, functions, connection, message) {
-        const actionRow = new ActionRowBuilder().addComponents(
-            new ButtonBuilder()
-                .setLabel("Next Page")
-                .setCustomId("nextpage")
-                .setStyle(1),
-        );
-        const embed = await functions.createEmbed(functions, "Test", "Test", null);
-        const msg = await message.channel.send({ embeds: [embed], components: [actionRow] });
-        //TODO: Finish command
+
+        const data = await functions.runQuery(functions, connection, `SELECT serverID, SUM(points) as total_points FROM points WHERE gameID >= 0  GROUP BY serverID ORDER BY total_points DESC LIMIT 10`, false);
+        let servers = "";
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            let serverName = await client.guilds.cache.get(data[i].serverID);
+            if(!serverName) serverName = { name: "Unknown" };
+            if (i <= 2) {
+                servers = servers + "```" + `${i + 1}. ${serverName.name} - ${data[i].total_points}` + "```\n";
+            } else {
+                servers = servers + `${i + 1}. ${serverName.name} - ${data[i].total_points}\n`;
+            }
+
+        }
+        const embed = await functions.createEmbed(functions, "Leaderboard Servers", servers, null);
+        const msg = await message.channel.send({ embeds: [embed] });
+
     }
 }
