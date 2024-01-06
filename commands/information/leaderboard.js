148ec28fddaf3643a025be456e8406c4e671eed0
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2024-01-06 18:26:18 +0100

    Add medals to leaderboard display

--- commands/information/leaderboard.js
+++ commands/information/leaderboard.js
@@ -9,15 +9,19 @@ module.exports = {
         permissions: 0,
     },
     run: async function (client, functions, connection, message) {
-
+        const medals = {
+            1: "🥇",
+            2: "🥈",
+            3: "🥉"
+        }
         const data = await functions.runQuery(functions, connection, `SELECT serverID, SUM(points) as total_points FROM points WHERE gameID >= 0  GROUP BY serverID ORDER BY total_points DESC LIMIT 10`, false);
         let servers = "";
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
             let serverName = await client.guilds.cache.get(data[i].serverID);
             if(!serverName) serverName = { name: "Unknown" };
             if (i <= 2) {
-                servers = servers + "```" + `${i + 1}. ${serverName.name} - ${data[i].total_points}` + "```\n";
+                const medal = medals[i + 1];
+                servers = servers + "```" + `${medal} ${i + 1}. ${serverName.name} - ${data[i].total_points}` + "```\n";
             } else {
                 servers = servers + `${i + 1}. ${serverName.name} - ${data[i].total_points}\n`;
             }
