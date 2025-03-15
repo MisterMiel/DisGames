bcab13439fd622722c3fff0dbb8df895101864fb
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2025-03-15 18:50:14 +0100

    Switch database connection from mysql to mysql2 and enhance error logging

--- functions/database/databaseManager.js
+++ functions/database/databaseManager.js
@@ -1,41 +1,49 @@
-const mysql = require('mysql');
+const mysql = require('mysql2'); 
 require("dotenv").config();
 
 module.exports.createConnection = async (functions) => {
     functions.createLog("Trying to connect to database", false, true);
+    
     return new Promise((resolve, reject) => {
-
-        var connection = mysql.createConnection({
+        const connection = mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        })
+            database: process.env.DB_NAME,
+            authPlugins: {
+                mysql_clear_password: () => () => Buffer.from(process.env.DB_PASSWORD)
+            }
+        });
 
         connection.connect(err => {
-            if (err) throw err;
-        })
-        functions.createLog("Connected to database", false, false);
-        resolve(connection);
-    })
+            if (err) {
+                functions.createLog("Database connection failed: " + err.message, true, true);
+                return reject(err);
+            }
 
+            functions.createLog("Connected to database", false, false);
+            resolve(connection);
+        });
+    });
 };
 
-module.exports.runQuery = async (functions, connection, query, consoleLog) => {
-    if (consoleLog === undefined || consoleLog === true) { functions.createLog("Trying to run a query", false, true); }
+module.exports.runQuery = async (functions, connection, query, consoleLog = false) => {
+    if (consoleLog) { functions.createLog("Trying to run a query", false, true); }
+
     return new Promise((resolve, reject) => {
         connection.query(query, (err, result) => {
-            if (err) throw err;
-            if (consoleLog === undefined || consoleLog === true) {
-                functions.createLog("Query runned", false, true);
-                if (consoleLog) {
-                    functions.createLog("Query Output", true, false)
-                    console.log(result)
-                };
+            if (err) {
+                functions.createLog("Query failed: " + err.message, true, true);
+                return reject(err);
+            }
+
+            if (consoleLog) {
+                functions.createLog("Query executed successfully", false, true);
+                functions.createLog("Query Output", true, false);
+                console.log(result);
             }
 
             resolve(result);
-        })
-    })
-
+        });
+    });
 };
