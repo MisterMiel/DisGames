8fb91e01864d99d2c250b0f60f269d96d61a2738
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2025-03-15 17:53:11 +0100

    Add logging for logs folder path in uncaughtException handler

--- functions/general/onCrash.js
+++ functions/general/onCrash.js
@@ -6,6 +6,7 @@ const logsFolder = path.join(process.cwd(), './logs');
 process.on('uncaughtException', (err) => {
     let currentTime = new Date();
     let logsName = `ERROR__${currentTime.getDate()}-${currentTime.getMonth() + 1}-${currentTime.getFullYear()}__${currentTime.getHours()}_${currentTime.getMinutes()}_${currentTime.getSeconds()}.txt`;
+    console.log("🚀 ~ process.on ~ logsFolder:", logsFolder)
 
     if (!fs.existsSync(logsFolder)) {
        return console.error('Logs folder not found');
