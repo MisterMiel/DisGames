97c3f0b2e849c887835dd0bb73c10c4dc5872e5e
Mister_Miel <58117027+MisterMiel@users.noreply.github.com>
2025-03-15 17:44:39 +0100

    Add check for logs folder existence before logging errors

--- functions/general/onCrash.js
+++ functions/general/onCrash.js
@@ -7,9 +7,10 @@ process.on('uncaughtException', (err) => {
     let currentTime = new Date();
     let logsName = `ERROR__${currentTime.getDate()}-${currentTime.getMonth() + 1}-${currentTime.getFullYear()}__${currentTime.getHours()}_${currentTime.getMinutes()}_${currentTime.getSeconds()}.txt`;
 
-    fs.writeFileSync(path.join(__dirname, logsFolder, logsName), `${err.name}\n\n${err.stack}`);
+    if (!fs.existsSync(logsFolder)) {
+       return console.error('Logs folder not found');
+    }
+    fs.writeFileSync(path.join(logsFolder, logsName), `${err.name}\n\n${err.stack}`);
 
     console.log(`Error logged in ${logsName}`);
-
-    //process.exit(); 
 });
